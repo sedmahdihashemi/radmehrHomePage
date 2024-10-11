29b4f286ba864c09a55af1245c78320d76c9577f
sedmahdihashemi <155801810+sedmahdihashemi@users.noreply.github.com>
2024-10-11 16:19:01 +0330

    Add files via upload

---
+++ icons/MailIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function MailIcon() {
+  return (
+    <>
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" fill="#A58961">
+        <path d="M3.75 4C2.784 4 2 4.784 2 5.75L12 12L22 5.75C22 4.784 21.216 4 20.25 4L3.75 4 z M 2 7.7519531L2 18C2 19.105 2.895 20 4 20L20 20C21.105 20 22 19.105 22 18L22 7.7519531L12 14L2 7.7519531 z" fill="#A58961" />
+      </svg>
+    </>
+  )
+}
